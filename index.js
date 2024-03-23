9c4bace515bfe311f7be5cf3d043f857581bdcf3
Caesar <ignatiuscaesar16@gmail.com>
2024-03-23 11:11:20 +0300

    Edits the js file

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 // Code your solution in this file!
+// Array of driver names for Scuber service
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
 //ReturnFirstTwoDrivers function
 const returnFirstTwoDrivers = function(drivers) {
     return drivers.slice(0, 2);
 };
+console.log(returnFirstTwoDrivers(drivers));
 
 //ReturnLastTwoDrivers function
 const returnLastTwoDrivers = function(drivers) {
     return drivers.slice(-2);
 };
+console.log(returnLastTwoDrivers(drivers));
 
 //SelectingDrivers array
 const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+console.log(selectingDrivers[0](drivers))
+console.log(selectingDrivers[1](drivers))
 
 //CreateFareMultiplier function
 const createFareMultiplier = function(multiplier) {
@@ -25,7 +32,16 @@ const fareDoubler = createFareMultiplier(2);
 //FareTripler function
 const fareTripler = createFareMultiplier(3);
 
+// Test the fareDoubler with a fare of 10
+console.log(fareDoubler(10));
+
+// Test the fareTripler with a fare of 10
+console.log(fareTripler(10));
+
 //SelectDifferentDrivers function
 const selectDifferentDrivers = function(drivers, whichDrivers) {
     return whichDrivers(drivers);
 };
+console.log(selectDifferentDrivers(drivers, returnFirstTwoDrivers));
+console.log(selectDifferentDrivers(drivers, returnLastTwoDrivers));
+
